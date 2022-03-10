e31dfe3b1007ead27402eca6fe8c0b97c8ef5e6f
yuang <yuang50915@gmail.com>
2022-03-10 21:20:52 +0800

    联系Comparable接口并测试。

--- src/main/java/com/jiangyan/algorithm/sort/Student.java
+++ src/main/java/com/jiangyan/algorithm/sort/Student.java
@@ -1,9 +1,30 @@
 package com.jiangyan.algorithm.sort;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * 需求：
- *
+ *      定义学生类Student，有name和age两个属性，通过Comparable接口比较
+ *      定义测试类，在测试方法中Comparable getMax(Comparable c1, Comparable c2)完成测试
  * @author zhang
  */
-public class Student {
+@Data
+@AllArgsConstructor
+@ToString
+public class Student implements Comparable<Student>{
+    private String userName;
+    private int age;
+
+
+    /**
+     * 重写比较规则。
+     * @param o 要比较的对象
+     * @return 大小，正为大于
+     */
+    @Override
+    public int compareTo(Student o) {
+        return this.getAge() - o.getAge();
+    }
 }
